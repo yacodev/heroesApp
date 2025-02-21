9ea9424dfc495edfe425ed2f146e1412d4e4d9d9
yacodev <cyaco33@gmail.com>
2025-02-21 09:50:10 -0500

    add validations

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: false,
   templateUrl: './login-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class LoginPageComponent {
-
+  constructor(private authService: AuthService, private router: Router) {}
+  onLogin() {
+    this.authService.login('fernando', '123').subscribe((user) => {
+      this.router.navigate(['/']);
+    });
+  }
 }
